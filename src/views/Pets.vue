1de9aa90e624190471a47a1812f12ffdd1767885
Thanasis <athan.gouf@gmail.com>
2019-05-23 19:55:49 +0300

    First push

---
+++ src/views/Pets.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>{{ animal.name }} ({{$route.params.species}})</h1>
+    <p>Age: {{ animal.age }} years old</p>
+    <p>Breed: {{ animal.breed }}</p>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  data () {
+    return {
+      animal: {}
+    }
+  },
+  computed: {
+    ...mapState([
+      'cats',
+      'dogs'
+    ])
+  },
+  mounted () {
+    const species = this.$route.params.species
+    const id = this.$route.params.id
+    const animal = this[species][id]
+    this.animal = animal
+  }
+}
+</script>
